7967edece3d117fc730d6613a9ebd202782be8da
Brad Petersen <brad.petersen7@gmail.com>
2020-07-04 07:37:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player = "X")
-  board[index] = current_player
+  board[index.to_i] = current_player
 end
 
 def valid_move? (board, index)
